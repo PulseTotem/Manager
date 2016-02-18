a56f918cc9daea6418c42fc89e2b164eaff748b4
Christian Brel <ch.brel@gmail.com>
2016-02-18 12:20:17 +0100

    refs MANAGER-1 : Begin structure for Manager.

---
+++ app/common/controllers/menu.js
@@ -0,0 +1,48 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name PulseTotemCommon.controller:MenuCtrl
+ * @description
+ * # MenuCtrl
+ * Controller of the PulseTotemCommon
+ */
+angular.module('PulseTotemCommon')
+    .controller('PulseTotemCommon.MenuCtrl', ['$rootScope', '$scope', '$translate', 'backendSocket', '$cookies', '$location', '$anchorScroll', 'CONSTANTS', '$mdSidenav', function ($rootScope, $scope, $translate, backendSocket, $cookies, $location, $anchorScroll, CONSTANTS, $mdSidenav) {
+
+        $rootScope.langKey = $translate.use();
+
+        $scope.changeLanguage = function (langKey) {
+          $rootScope.langKey = langKey;
+          $translate.use(langKey);
+        };
+
+        $scope.logout = function() {
+          $rootScope.user = {};
+
+          delete($cookies.tmpPulseTotemToken);
+          delete($cookies.pulseTotemToken);
+
+          backendSocket.exit();
+
+          if (!$rootScope.$$phase) {
+            $rootScope.$apply(function () {
+              $location.path(CONSTANTS.homeRoute);
+            });
+          } else {
+            $location.path(CONSTANTS.homeRoute);
+          }
+        };
+
+        $scope.toggleLeft = buildToggler('left');
+        /**
+         * Build handler to open/close a SideNav;
+         */
+        function buildToggler(navID) {
+          return function() {
+            $mdSidenav(navID)
+              .toggle();
+          }
+        }
+
+    }]);
---
+++ app/main/routes.js
@@ -0,0 +1,38 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name pulsetotemManagerApp
+ * @description
+ * # routes
+ *
+ * Define routes available in application.
+ */
+angular
+  .module('pulsetotemManagerApp')
+  .config(['$routeProvider', function ($routeProvider) {
+    $routeProvider
+      // All other stuff
+      .when('/', {
+        templateUrl: '../common/views/home.html',
+        controller: 'PulseTotemCommon.HomeCtrl',
+        reloadOnSearch: false
+      })
+
+      // Routes for authentication
+      .when('/login', {
+        templateUrl: '../common/views/login.html',
+        controller: 'PulseTotemCommon.LoginCtrl'
+      })
+
+      // Routes for Dashboard
+      .when('/dashboard', {
+        templateUrl: '../common/views/login.html',
+        controller: 'PulseTotemCommon.LoginCtrl'
+      })
+
+      // All other stuff
+      .otherwise({
+        redirectTo: '/'
+      });
+  }]);
