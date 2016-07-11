611a754e4b0cc9f3a1e5012f822810cd38764f15
Christian Brel <ch.brel@gmail.com>
2016-07-11 20:46:17 +0200

    refs MANAGER-22 : Display list of teams with their members in CMS.

--- app/dashboard/controllers/main.js
+++ app/dashboard/controllers/main.js
@@ -8,10 +8,22 @@
  * Controller of the PulseTotemManagerDashboard
  */
 angular.module('PulseTotemManagerDashboard')
-  .controller('PulseTotemManagerDashboard.MainCtrl', ['$rootScope', function($rootScope){
+  .controller('PulseTotemManagerDashboard.MainCtrl', ['$rootScope', '$scope', 'Team', function($rootScope, $scope, Team){
     $rootScope.activeMenu = 'dashboard';
     $rootScope.activeNavbar = 'dashboard';
 
+    $scope.teams = [];
+    $scope.teamsLoaded = false;
+    $scope.loadTeams = function(){
+      Team.resource($rootScope.user.cmsAuthkey).query(function(teams) {
+        $scope.teams = [];
+        teams.forEach(function(team) {
+          $scope.teams.push(team);
+        });
+        $scope.teamsLoaded = true;
+      });
+    };
 
+    $scope.loadTeams();
 
   }]);
