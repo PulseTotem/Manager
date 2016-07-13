b52d6c40f34aa99b4cfae21260d6607a3f7d22bd
Christian Brel <ch.brel@gmail.com>
2016-07-13 16:50:49 +0200

    refs MANAGER-23 : List all collections of users and manage moving collection to a Team.

--- app/dashboard/controllers/main.js
+++ app/dashboard/controllers/main.js
@@ -8,22 +8,88 @@
  * Controller of the PulseTotemManagerDashboard
  */
 angular.module('PulseTotemManagerDashboard')
-  .controller('PulseTotemManagerDashboard.MainCtrl', ['$rootScope', '$scope', 'Team', function($rootScope, $scope, Team){
+  .controller('PulseTotemManagerDashboard.MainCtrl', ['$rootScope', '$scope', 'Team', 'User', 'PhotosCollection', 'VideosCollection', 'NewsCollection', "$http", "CONSTANTS", function($rootScope, $scope, Team, User, PhotosCollection, VideosCollection, NewsCollection, $http, CONSTANTS){
     $rootScope.activeMenu = 'dashboard';
     $rootScope.activeNavbar = 'dashboard';
 
     $scope.teams = [];
-    $scope.teamsLoaded = false;
     $scope.loadTeams = function(){
       Team.resource($rootScope.user.cmsAuthkey).query(function(teams) {
         $scope.teams = [];
         teams.forEach(function(team) {
           $scope.teams.push(team);
         });
-        $scope.teamsLoaded = true;
       });
     };
 
     $scope.loadTeams();
+
+    $scope.loadPhotosCollections = function(){
+      PhotosCollection.resource($rootScope.user.cmsAuthkey).query({userid: $rootScope.user.cmsId}, function(collections) {
+        $scope.collections = [];
+        collections.forEach(function(collection) {
+          if(! collection.autogenerate) {
+            collection['coverPath'] = '/images/cms/photos/empty.png';
+            if (collection.cover != null) {
+              collection['coverPath'] = CONSTANTS.cmsUrl + CONSTANTS.cmsPhotosPath + collection.cover.id + '/raw?size=medium';
+            }
+
+            $scope.collections.push(collection);
+          }
+        });
+        $scope.collectionsLoaded = true;
+        $scope.actionLoading = "";
+      });
+    };
+
+    $scope.usersCollections = [];
+    $scope.loadCollections = function(){
+      $scope.users.forEach(function(user) {
+        $scope.usersCollections[user.id] = {};
+        PhotosCollection.resource($rootScope.user.cmsAuthkey).query({userid: user.id}, function(collections) {
+          $scope.usersCollections[user.id]["photosCollections"] = [];
+
+          collections.forEach(function(collection) {
+            $scope.usersCollections[user.id]["photosCollections"].push(collection);
+          });
+        });
+
+        VideosCollection.resource($rootScope.user.cmsAuthkey).query({userid: user.id}, function(collections) {
+          $scope.usersCollections[user.id]["videosCollections"] = collections;
+        });
+
+        NewsCollection.resource($rootScope.user.cmsAuthkey).query({userid: user.id}, function(collections) {
+          $scope.usersCollections[user.id]["newsCollections"] = collections;
+        });
+
+      });
+    };
+
+    $scope.users = [];
+    $scope.loadUsers = function(){
+      User.resource($rootScope.user.cmsAuthkey).query(function(users) {
+        $scope.users = [];
+
+        users.forEach(function(user) {
+          $scope.users.push(user);
+        });
+
+        $scope.loadCollections();
+      });
+    };
+
+    $scope.loadUsers();
+
+    $scope.movePhotosToTeam = function(coll, teamJSON) {
+      var team = JSON.parse(teamJSON);
+
+      $http.post(CONSTANTS.cmsUrl + CONSTANTS.cmsPhotosCollectionsPath + coll.id + "/" + CONSTANTS.cmsTeamsPath + team.id, {})
+        .success(function (data, status, headers, config) {
+          alert('Ok');
+        })
+        .error(function (data, status, headers, config) {
+          console.log("fail during move photos coll");
+        });
+    }
 
   }]);
